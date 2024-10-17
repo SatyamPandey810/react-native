5fe26588d5a0f5c51004f1e57342cffa4623059d
satyam pandey <140078019+SatyamPandey810@users.noreply.github.com>
2024-10-17 18:25:01 +0530

    add.

--- model/app/Home.tsx
+++ model/app/Home.tsx
@@ -2,14 +2,15 @@ import { View, Text, Button, Modal } from 'react-native'
 import React, { useState } from 'react'
 
 const Home = () => {
-    const [visible,setVisible]=useState(false)
+    const [visible, setVisible] = useState(false)
 
     return (
         <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-            <Button title='open model' onPress={()=>setVisible(true)}/>
+            <Button title='open model' onPress={() => setVisible(true)} />
             <Modal visible={visible} transparent animationType='slide' >
                 <View style={{ backgroundColor: "red", height: 300 }}
-                ><Button title="close modal" onPress={()=>setVisible(false)}/>
+                >
+                    <Button title="close modal" onPress={() => setVisible(false)} />
                 </View>
             </Modal>
         </View>
